58476d757fbe4b423aac66725de123e188465dfd
Juan Ospina <juanospina11@gmail.com>
2023-02-22 16:46:02 -0500

    catching errors - 1:48:32

--- TutorialProject1/Program.cs
+++ TutorialProject1/Program.cs
@@ -1,16 +1,33 @@
-﻿public class TutorialProject
+﻿using System;
+
+namespace HelloWorld
 {
-    public static void Main()
+    class Program
     {
-        float vat = 19f;
-        float productA = 3.99f;
-        float productB = 11.99f;
-        float productC = 5.75f;
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Please input 2 numbers to multiply");
+           string input1 = Console.ReadLine();
+           string input2 = Console.ReadLine();
+            if(Int32.TryParse(input1, out int num1) && Int32.TryParse(input2, out int num2)) { 
+            Console.WriteLine(Add(num1, num2));
+            } 
+            else
+            {
+                Console.WriteLine("One of your inputs is not a number!");
+            }
 
-        float summed = productA + productB + productC;
-        float totalTax = (summed / 100) * vat;
+            Console.Read();
+            
+           
+        }
 
-        summed += totalTax;
-        Console.WriteLine(summed);
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+      
+
     }
 }
+
