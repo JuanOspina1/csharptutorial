631acd53e0a921f1a1a71db687bc23b60f6ede6c
Juan Ospina <juanospina@juans-mbp.lan>
2023-02-21 21:15:56 -0500

    initial commit - 55:53mins

---
+++ TutorialProject1/Program.cs
@@ -0,0 +1,16 @@
+﻿public class TutorialProject
+{
+    public static void Main()
+    {
+        float vat = 19f;
+        float productA = 3.99f;
+        float productB = 11.99f;
+        float productC = 5.75f;
+
+        float summed = productA + productB + productC;
+        float totalTax = (summed / 100) * vat;
+
+        summed += totalTax;
+        Console.WriteLine(summed);
+    }
+}
